6da87cf8e3564648dfd8e3dcf1d84dcfa703d02f
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-02-13 15:20:28 +0530

    used block listener

--- lib/main.dart
+++ lib/main.dart
@@ -42,59 +42,74 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
-              if (state.counterValue < 0) {
-                return Text(
-                  'BRB , NEgATIVE'+state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              } else if (state.counterValue % 2 == 0) {
-                return Text(
-                  'YAAY,EVEN NUMBER' + state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              } else if (state.counterValue == 5) {
-                return Text(
-                  'HMM ,NUMBER '+state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              } else {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              }
-            }),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.wasIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Incremented!'),
+              duration: Duration(milliseconds: 300),
+            ));
+          } else if (state.wasIncremented == false) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Decremented!'),
+              duration: Duration(milliseconds: 300),
+            ));
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'BRB , NEgATIVE' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'YAAY,EVEN NUMBER' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'HMM ,NUMBER ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+              }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
